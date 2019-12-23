ed129983c0bc227ae4cc2c658bad035d5106e8e4
Sandip Shinde <49554858+sandip-shinde@users.noreply.github.com>
2019-12-23 16:58:55 +0530

    add all file
    add File

---
+++ RabbitMQConsumer/RabbitMQConsumer/Program.cs
@@ -0,0 +1,33 @@
+using RabbitMQ.Client;
+using System;
+
+namespace RabbitMQConsumer
+{
+    class Program
+    {
+        private const string UName = "guest";
+        private const string Pwd = "guest";
+        private const string HName = "localhost";
+        static void Main(string[] args)
+        {
+            // The code provided will print ‘Hello World’ to the console.
+            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
+            Console.WriteLine("Hello World!");
+            ConnectionFactory connectionFactory = new ConnectionFactory
+            {
+                HostName = HName,
+                UserName = UName,
+                Password = Pwd,
+            };
+            var connection = connectionFactory.CreateConnection();
+            var channel = connection.CreateModel();
+            // accept only one unack-ed message at a time
+            // uint prefetchSize, ushort prefetchCount, bool global
+            channel.BasicQos(0, 1, false);
+            MessageReceiver messageReceiver = new MessageReceiver(channel);
+            channel.BasicConsume("topic.bombay.queue", false, messageReceiver);
+            Console.ReadLine();
+
+        }
+    }
+}
